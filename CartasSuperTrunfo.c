d26f136b977ac293a58491688f5ca025daa1a103
Cursos de Tecnologia da Informação <164575969+Cursos-de-Teconologia-da-Informacao@users.noreply.github.com>
2024-08-27 17:43:01 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das cartas
+// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+
+int main() {
+  // Área para definição das variáveis para armazenar as propriedades das cidades
+
+  // Área para entrada de dados
+
+  // Área para exibição dos dados da cidade
+
+return 0;
+}
